5c665aecdb1fa6b0e487dc14a1a098aea81b4ac6
Craig <crdavies@hotmail.co.uk>
2016-10-10 15:41:00 +0100

    it's a message!

--- dbfile1.php
+++ dbfile1.php
@@ -1,31 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SQL Query Generation</title>
+</head>
+<body>
 <?php
+//DEBUG HEADER
+print('<p>ERRORS (if any):</p>');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+print('<hr>');
+//-----------------------
 /**
  * Created by PhpStorm.
- * User: 0702555
+ * User: 1406519
  * Date: 10/10/2016
- * Time: 14:24
+ * Time: 14:02
  */
-
-include ("dbconnect.php");
-
-$db;
-
-        
-if($db->connect_errno) {
-    die('connectfailed['.$db->connect_error.']');
-}
-
-else {
-    echo" lol";
-}
-
-$sql_query="SELECT * FROM superheroes WHERE superpower LIKE '%aser%'";
-$result = $db->query($sql_query);
-
-while($row=$result->fetch_array()){
-    echo "<p>".$row['superheroName']."</p>";
-}
-echo "wat";
-$result->close();
-
+print('<strong>Server Conn. Stat.</strong>');
+include("db_connect.php"); // INCLUDE THE CONNECTION FILE
 ?>
+<hr>
+<div>
+    <?php
+    // GENERATE SQL QUERIES
+    print('<p><strong>Query 1</strong></p>');
+    // Create the SQL query
+    $sql_query = "SELECT * FROM marvelmovies";
+    // Run the sql on the DB
+    $res = mysqli_query($db_conn, $sql_query);
+    while($row = $res->fetch_array()){
+        $movie_title = $row['title'];
+        echo '<p>'.$movie_title.'</p>';
+    }
+    print('<hr>');
+    // Query 2
+    print('<p><strong>Query 2</strong></p>');
+    $sql_query = "SELECT * FROM marvelmovies WHERE productionStudio = 'Marvel Studios'";
+    // Run the sql on the DB
+    $res = mysqli_query($db_conn, $sql_query);
+    while($row = $res->fetch_array()){
+        $movie_title = $row['title'];
+        echo '<p>'.$movie_title.'</p>';
+    }
+    print('<hr>');
+    // Query 3
+    print('<p><strong>Query 3</strong></p>');
+    $sql_query = "SELECT * FROM marvelmovies WHERE yearReleased >= 2010";
+    // Run the sql on the DB
+    $res = mysqli_query($db_conn, $sql_query);
+    while($row = $res->fetch_array()){
+        $movie_title = $row['title'];
+        echo '<p>'.$movie_title.'</p>';
+    }
+    print('<hr>');
+    // Query 4
+    print('<p><strong>Query 4</strong></p>');
+    $sql_query = "SELECT * FROM marvelmovies WHERE title LIKE 'X-Men%'";
+    // Run the sql on the DB
+    $res = mysqli_query($db_conn, $sql_query);
+    while($row = $res->fetch_array()){
+        $movie_title = $row['title'];
+        echo '<p>'.$movie_title.'</p>';
+    }
+    ?>
+</div>
+</body>
+</html>
