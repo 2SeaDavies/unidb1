dbf154286fc9ecb25cf940e4d8af83daf85464f3
Craig <crdavies@hotmail.co.uk>
2016-10-17 14:42:05 +0100

    it's a message!

---
+++ hello_printer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SQL Query Generation</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0702555
+ * Date: 17/10/2016
+ * Time: 14:39
+ */
+
+
+$planet = $_GET["planet"];
+echo "hello ".$planet;
+?>
+</body>
+</html>
---
+++ hello_selector.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SQL Query Generation</title>
+</head>
+<body>
+<?php
+
+echo "<a href='www.hello_printer.php?planet=Earth'>Earth </a>";
+echo "<a href='www.hello_printer.php?planet=Mars'>Mars </a>";
+echo "<a href='www.hello_printer.php?planet=Venus'>Venus </a>";
+
+
+
+
+
+?>
+</body>
+</html>
